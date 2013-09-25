dc45837a05e52ee18fb078c7d52193ce19cb2012
Magnus Achim Deininger <magnus@ef.gy>
2013-09-25 15:40:27 +0100

    updated copyright statements

--- include/ef.gy/http.h
+++ include/ef.gy/http.h
@@ -5,7 +5,7 @@
 */
 
 /*
- * Copyright (c) 2012, ef.gy Project Members
+ * Copyright (c) 2012-2013, ef.gy Project Members
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

90727f073ae912a84a0dfa64144b672931123230
Magnus Achim Deininger <magnus@ef.gy>
2014-05-20 22:22:10 +0100

    update repository location

--- include/ef.gy/http.h
+++ include/ef.gy/http.h
@@ -27,7 +27,7 @@
  * THE SOFTWARE.
  *
  * \see Project Documentation: http://ef.gy/documentation/libefgy
- * \see Project Source Code: http://git.becquerel.org/jyujin/libefgy.git
+ * \see Project Source Code: https://github.com/ef-gy/libefgy.git
  */
 
 #if !defined(EF_GY_HTTP_H)

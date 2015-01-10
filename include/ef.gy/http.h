09f1f66e693ae6003c40f037cc8c35612a6cbe6e
Magnus Deininger <mdeininger@google.com>
2015-01-10 14:25:37 +0100

    made JSON handlers much less annoying to use; updated copyright notices

--- include/ef.gy/http.h
+++ include/ef.gy/http.h
@@ -6,7 +6,7 @@
  * using this header.
  *
  * \copyright
- * Copyright (c) 2012-2014, ef.gy Project Members
+ * Copyright (c) 2012-2015, ef.gy Project Members
  * \copyright
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

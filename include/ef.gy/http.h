233fd5fdc67d8747f37546fccbfed98790b5fb49
Magnus Achim Deininger <magnus@ef.gy>
2015-03-15 10:34:17 +0000

    further cleanups in the http code

--- include/ef.gy/http.h
+++ include/ef.gy/http.h
@@ -59,17 +59,15 @@ namespace net {
 namespace http {
 /**\brief HTTP state classes
  *
- * Contains state classes which are used to keep track of the data
- * that a request processor functor needs to respond to a client's
- * HTTP request.
+ * Contains state classes which are used to keep track of the data that a
+ * request processor functor needs to respond to a client's HTTP request.
  */
 namespace state {
 /**\brief Stub state class
  *
- * This is the default state class, which contains no data at
- * all. Many kinds of requests do not need additional data to
- * process requests, and they'll be perfectly fine with this
- * state class.
+ * This is the default state class, which contains no data at all. Many kinds of
+ * requests do not need additional data to process requests, and they'll be
+ * perfectly fine with this state class.
  */
 class stub {
 public:
@@ -88,12 +86,12 @@ class server;
 
 /**\brief Session wrapper
  *
- * Used by the server to keep track of all the data associated with
- * a single, asynchronous client connection.
+ * Used by the server to keep track of all the data associated with a single,
+ * asynchronous client connection.
  *
  * \tparam requestProcessor The functor class to handle requests.
- * \tparam stateClass       Data class needed to keep track of the
- *                          resources used by the requestProcessor.
+ * \tparam stateClass       Data class needed to keep track of the resources
+ *                          used by the requestProcessor.
  * \tparam maxContentLength Maximum size of incoming body data.
  */
 template <typename base, typename requestProcessor,
@@ -103,37 +101,22 @@ class session {
 protected:
   /**\brief HTTP request parser status
    *
-   * Contains the current status of the request parser for the
-   * current session.
+   * Contains the current status of the request parser for the current session.
    */
   enum {
     stRequest, /**< Request received. */
-    stHeader,
-    /**< Currently parsing the
-     *   request header.
-     */
-    stContent,
-    /**< Currently parsing the
-     *   request body.
-     */
-    stProcessing,
-    /**< Currently processing the
-     *   request.
-     */
-    stErrorContentTooLarge,
-    /**< Error: Content-Length is
-     *   greater than
-     *   maxContentLength
-     */
-    stShutdown /**< Will shut down the
-                *   connection now.
-                */
+    stHeader, /**< Currently parsing the request header. */
+    stContent, /**< Currently parsing the request body. */
+    stProcessing, /**< Currently processing the request. */
+    stErrorContentTooLarge, /**< Error: Content-Length is greater than
+                             * maxContentLength */
+    stShutdown /**< Will shut down the connection now. Set in the destructor. */
   } status;
 
   /**\brief Case-insensitive comparison functor
    *
-   * A simple functor used by the attribute map to compare
-   * strings without caring for the letter case.
+   * A simple functor used by the attribute map to compare strings without
+   * caring for the letter case.
    */
   class caseInsensitiveLT
       : private std::binary_function<std::string, std::string, bool> {
@@ -148,13 +131,10 @@ protected:
      *
      * Compares two strings case-insensitively.
      *
-     * \param[in] a The first of the two strings to
-     *              compare.
-     * \param[in] b The second of the two strings to
-     *              compare.
+     * \param[in] a The first of the two strings to compare.
+     * \param[in] b The second of the two strings to compare.
      *
-     * \returns 'true' if the first string is less than
-     *          the second.
+     * \returns 'true' if the first string is less than the second.
      */
     bool operator()(const std::string &a, const std::string &b) const {
       return std::lexicographical_compare(a.begin(), a.end(), b.begin(),
@@ -166,32 +146,31 @@ protected:
 public:
   /**\brief Stream socket
    *
-   * This is the asynchronous I/O socket that is used to
-   * communicate with the client.
+   * This is the asynchronous I/O socket that is used to communicate with the
+   * client.
    */
   typename base::socket socket;
 
   /**\brief The request's HTTP method
    *
-   * Contains the HTTP method that was used in the request,
-   * e.g. GET, HEAD, POST, PUT, etc.
+   * Contains the HTTP method that was used in the request, e.g. GET, HEAD,
+   * POST, PUT, etc.
    */
   std::string method;
 
   /**\brief Requested HTTP resource location
    *
-   * This is the location that was requested, e.g. / or
-   * /fortune or something like that.
+   * This is the location that was requested, e.g. / or /fortune or something
+   * like that.
    */
   std::string resource;
 
   /**\brief HTTP request headers
    *
-   * Contains all the headers that were sent along with the
-   * request. Things like Host: or DNT:. Uses a special
-   * case-insensitive sorting function for the map, so that
-   * you can query the contents without having to know the
-   * case of the headers as they were sent.
+   * Contains all the headers that were sent along with the request. Things
+   * like Host: or DNT:. Uses a special case-insensitive sorting function for
+   * the map, so that you can query the contents without having to know the case
+   * of the headers as they were sent.
    */
   std::map<std::string, std::string, caseInsensitiveLT> header;
 
@@ -203,41 +182,40 @@ public:
 
   /**\brief Auxiliary state
    *
-   * This is a reference to a state object, if one was passed
-   * to the constructor. Only useful for the user code to keep
-   * track of programme state - such as database connections -
-   * and completely ignored by the HTTP wrapper.
+   * This is a reference to a state object, if one was passed to the
+   * constructor. Only useful for the user code to keep track of programme state
+   * - such as database connections - and completely ignored by the HTTP wrapper.
    */
   stateClass *state;
 
   /**\brief Construct with I/O service
    *
-   * Constructs a session with the given asynchronous I/O
-   * service.
+   * Constructs a session with the given asynchronous I/O service.
    */
   session(asio::io_service &pIOService)
-      : socket(pIOService), status(stRequest), input() {}
+      : socket(pIOService), status(stRequest), input() {
+    std::cerr << "session created\n";
+  }
 
   /**\brief Destructor
    *
-   * Closes the socket, cancels all remaining requests and
-   * sets the status to stShutdown.
+   * Closes the socket, cancels all remaining requests and sets the status to
+   * stShutdown.
    */
   ~session(void) {
     status = stShutdown;
     socket.shutdown(base::socket::shutdown_both);
     socket.cancel();
     socket.close();
+    std::cerr << "session destroyed\n";
   }
 
   /**\brief Start processing
    *
-   * Starts processing the incoming request. As a side-effect,
-   * this method also sets the auxiliary state which is later
-   * used by the request processor.
+   * Starts processing the incoming request. As a side-effect, this method also
+   * sets the auxiliary state which is later used by the request processor.
    *
-   * \param[in] state The auxiliary state for the request
-   *                  processor.
+   * \param[in] state The auxiliary state for the request processor.
    */
   void start(stateClass *state) {
     this->state = state;
@@ -246,65 +224,63 @@ public:
 
   /**\brief Send reply
    *
-   * Used by the processing code once it is known how to
-   * answer the request contained in this object. The code
-   * will automatically add a correct Content-Length header,
-   * but further headers may be added using the free-form
+   * Used by the processing code once it is known how to answer the request
+   * contained in this object. The code will automatically add a correct
+   * Content-Length header, but further headers may be added using the free-form
    * header parameter. Headers are not checked for validity.
    *
-   * \note The code will always reply with an HTTP/1.1 reply,
-   *       regardless of the version in the request. If this
-   *       is a concern for you, put the server behind an
-   *       an nginx instance, which should fix up the output
-   *       as necessary.
+   * \note The code will always reply with an HTTP/1.1 reply, regardless of the
+   *       version in the request. If this is a concern for you, put the server
+   *       behind an an nginx instance, which should fix up the output as
+   *       necessary.
    *
    * \param[in] status The status to return.
    * \param[in] header Any additional headers to be sent.
-   * \param[in] body   The response body to send back to the
-   *                   client.
+   * \param[in] body   The response body to send back to the client.
    */
   void reply(int status, const std::string &header, const std::string &body) {
     std::stringstream reply;
-    reply << "HTTP/1.1 " << status << " NA\r\nContent-Length: " << body.length()
-          << "\r\n" + header + "\r\n" + body;
 
-    if (status < 400) {
-      asio::async_write(socket, asio::buffer(reply.str()),
-                        [&](std::error_code ec, std::size_t length) {
-        handleWrite(ec);
-      });
-    } else {
-      asio::async_write(socket, asio::buffer(reply.str()),
-                        [&](std::error_code ec, std::size_t length) {
-        handleWriteClose(ec);
-      });
+    reply << "HTTP/1.1 " << status << " NA\r\n"
+          << "Content-Length: " << body.length() << "\r\n";
+
+    /* we automatically close connections when an error code is sent. */
+    if (status >= 400)
+    {
+      reply << "Connection: close\r\n";
     }
+
+    reply << header
+          << "\r\n" + body;
+
+    asio::async_write(socket, asio::buffer(reply.str()),
+                      [&](std::error_code ec, std::size_t length) {
+      handleWrite(status, ec);
+    });
   }
 
   /**\brief Send reply without custom headers
    *
-   * Wraps around the 3-parameter reply() function, so that
-   * you don't have to specify an empty header parameter if
-   * you don't intend to set custom headers.
+   * Wraps around the 3-parameter reply() function, so that you don't have to
+   * specify an empty header parameter if you don't intend to set custom
+   * headers.
    *
    * \param[in] status The status to return.
-   * \param[in] body   The response body to send back to the
-   *                   client.
+   * \param[in] body   The response body to send back to the client.
    */
   void reply(int status, const std::string &body) { reply(status, "", body); }
 
 protected:
   /**\brief Read more data
    *
-   * Called by ASIO to indicate that new data has been read
-   * and can now be processed.
+   * Called by ASIO to indicate that new data has been read and can now be
+   * processed.
    *
-   * The actual processing for the header is done with a set
-   * of regexen, which greatly simplifies the header parsing.
+   * The actual processing for the header is done with a set of regexen, which
+   * greatly simplifies the header parsing.
    *
    * \param[in] error             Current error state.
-   * \param[in] bytes_transferred The amount of data that has
-   *                              been read.
+   * \param[in] bytes_transferred The amount of data that has been read.
    */
   void handleRead(const std::error_code &error, size_t bytes_transferred) {
     if (status == stShutdown) {
@@ -415,20 +391,20 @@ protected:
 
   /**\brief Asynchronouse write handler
    *
-   * Decides whether or not things need to be written to the
-   * stream, or if things need to be read instead.
+   * Decides whether or not things need to be written to the stream, or if
+   * things need to be read instead.
    *
-   * Automatically deletes the object on errors - which also
-   * closes the connection automagically.
+   * Automatically deletes the object on errors - which also closes the
+   * connection automagically.
    *
    * \param[in] error Current error state.
    */
-  void handleWrite(const std::error_code &error) {
+  void handleWrite(int statusCode, const std::error_code &error) {
     if (status == stShutdown) {
       return;
     }
 
-    if (!error) {
+    if (!error && (statusCode < 400)) {
       if (status == stProcessing) {
         status = stRequest;
         read();
@@ -438,31 +414,12 @@ protected:
     }
   }
 
-  /**\brief Asynchronouse write and close handler
-   *
-   * Decides whether or not things need to be written to the
-   * stream, or if things need to be read instead.
-   *
-   * Automatically deletes the object on errors - which also
-   * closes the connection automagically.
-   *
-   * \param[in] error Current error state.
-   */
-  void handleWriteClose(const std::error_code &error) {
-    if (status == stShutdown) {
-      return;
-    }
-
-    delete this;
-  }
-
   /**\brief Asynchronouse read handler
    *
-   * Decides if things need to be read in and - if so - what
-   * needs to be read.
+   * Decides if things need to be read in and - if so - what needs to be read.
    *
-   * Automatically deletes the object on errors - which also
-   * closes the connection automagically.
+   * Automatically deletes the object on errors - which also closes the
+   * connection automagically.
    */
   void read(void) {
     switch (status) {
@@ -494,15 +451,15 @@ protected:
 
   /**\brief Predicate: has the request been fully parsed?
    *
-   * Determines whether a request has fully been parsed and if
-   * the connection can safely be severed. Will be nonzero if
-   * an error occurred or if there is still data to be read.
+   * Determines whether a request has fully been parsed and if the connection
+   * can safely be severed. Will be nonzero if an error occurred or if there is
+   * still data to be read.
    *
    * \param[in] error             Current error context.
    * \param[in] bytes_transferred How much data has been read.
    *
-   * \returns 0 when the request has been fully parsed and the
-   *          connection can now be dropped.
+   * \returns 0 when the request has been fully parsed and the connection can
+   *          now be dropped.
    */
   std::size_t contentReadP(const std::error_code &error,
                            std::size_t bytes_transferred) {
@@ -513,34 +470,34 @@ protected:
 
   /**\brief Name of the last parsed header
    *
-   * This is the name of the last header line that was parsed.
-   * Used with multi-line headers.
+   * This is the name of the last header line that was parsed. Used with
+   * multi-line headers.
    */
   std::string lastHeader;
 
   /**\brief Content length
    *
-   * This is the value of the Content-Length header. Used when
-   * parsing a request with a body.
+   * This is the value of the Content-Length header. Used when parsing a request
+   * with a body.
    */
   std::size_t contentLength;
 
   /**\brief ASIO input stream buffer
    *
-   * This is the stream buffer that the object is reading
-   * from.
+   * This is the stream buffer that the object is reading from.
    */
   asio::streambuf input;
 };
 
 /**\brief HTTP server wrapper
  *
- * Contains the code that accepts incoming HTTP requests and
- * dispatches asynchronous processing.
+ * Contains the code that accepts incoming HTTP requests and dispatches
+ * asynchronous processing.
  *
+ * \tparam base             The socket class, e.g. asio::ip::tcp
  * \tparam requestProcessor The functor class to handle requests.
- * \tparam stateClass       Data class needed to keep track of the
- *                          resources used by the requestProcessor.
+ * \tparam stateClass       Data class needed to keep track of the resources
+ *                          used by the requestProcessor.
  * \tparam maxContentLength Maximum size of incoming body data.
  */
 template <typename base, typename requestProcessor, typename stateClass,
@@ -549,11 +506,11 @@ class server {
 public:
   /**\brief Initialise with IO service
    *
-   * Default constructor which binds an IOService to a UNIX
-   * socket. The socket is bound asynchronously.
+   * Default constructor which binds an IOService to a socket endpoint that was
+   * passed in. The socket is bound asynchronously.
    *
    * \param[out] pIOService IO service to use.
-   * \param[in]  socket     UNIX socket to bind.
+   * \param[in]  endpoint   Endpoint for the socket to bind.
    * \param[in]  stateData  Data to pas to the state class.
    */
   server(asio::io_service &pIOService, typename base::endpoint &endpoint,
@@ -566,8 +523,8 @@ public:
 protected:
   /**\brief Accept the next incoming connection
    *
-   * This function creates a new, blank session to handle the
-   * next incoming request.
+   * This function creates a new, blank session to handle the next incoming
+   * request.
    */
   void startAccept(void) {
     session<base, requestProcessor, stateClass> *newSession =
@@ -581,15 +538,13 @@ protected:
 
   /**\brief Handle next incoming connection
    *
-   * Called by asio.hpp when a new inbound connection has
-   * been accepted; this will make the session parse the
-   * incoming request and dispatch it to the request processor
-   * specified as a template argument.
+   * Called by asio.hpp when a new inbound connection has been accepted; this
+   * will make the session parse the incoming request and dispatch it to the
+   * request processor specified as a template argument.
    *
-   * \param[in] newSession The blank session object that was
-   *                       created by startAccept().
-   * \param[in] error      Describes any error condition that
-   *                       may have occurred.
+   * \param[in] newSession The blank session object that was created by
+   *                       startAccept().
+   * \param[in] error      Describes any error condition that may have occurred.
    */
   void handleAccept(
       session<base, requestProcessor, stateClass, maxContentLength> *newSession,
@@ -605,27 +560,27 @@ protected:
 
   /**\brief IO service
    *
-   * Bound in the constructor to a asio.hpp IO service
-   * which handles asynchronous connections.
+   * Bound in the constructor to an asio.hpp IO service which handles
+   * asynchronous connections.
    */
   asio::io_service &IOService;
 
   /**\brief Socket acceptor
    *
-   * This is the acceptor which has been bound to the socket
-   * specified in the constructor.
+   * This is the acceptor which has been bound to the socket specified in the
+   * constructor.
    */
   typename base::acceptor acceptor;
 
   /**\brief Server state instance
    *
-   * Contains the global server state, which is passed to each
-   * new session upon being created.
+   * Contains the global server state, which is passed to each new session upon
+   * being created.
    */
   stateClass state;
 };
-};
-};
-};
+}
+}
+}
 
 #endif

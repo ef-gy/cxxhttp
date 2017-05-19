c00a19613ed58082c4828c12176933d322be3785
Magnus Deininger <magnus@ef.gy>
2017-05-19 22:46:53 +0000

    move the fake transport and session to a header for reuse by other tests (and downstream clients)

--- src/test-case/options.cpp
+++ src/test-case/options.cpp
@@ -1,7 +1,7 @@
-/* Test cases for the TRACE implementation.
+/* Test cases for the OPTIONS implementation.
  *
- * TRACE is a debugging tool built into HTTP that replies to any request that is
- * sent with the request and header lines that were sent.
+ * OPTIONS allows for querying allowed operations off a server, which helps
+ * clients in determining what to do with a given server.
  *
  * See also:
  * * Project Documentation: https://ef.gy/documentation/cxxhttp
@@ -16,16 +16,16 @@
 #include <ef.gy/test-case.h>
 
 #define ASIO_DISABLE_THREADS
-#define NO_DEFAULT_TRACE
+#define NO_DEFAULT_OPTIONS
 #include <cxxhttp/http-test.h>
-#include <cxxhttp/httpd-trace.h>
+#include <cxxhttp/httpd-options.h>
 
 using namespace cxxhttp;
 
-/* Test the trace handler.
+/* Test the options handler.
  * @log Test output stream.
  *
- * Does a full end-to-end exercise of the trace handler, with very controlled
+ * Does a full end-to-end exercise of the options handler, with very controlled
  * inputs.
  *
  * @return 'true' on success, 'false' otherwise.
@@ -33,42 +33,45 @@ using namespace cxxhttp;
 bool testTraceHandler(std::ostream &log) {
   struct sampleData {
     std::string request;
-    std::set<std::string> headers;
     unsigned status;
     http::headers header;
     std::string message;
   };
 
   std::vector<sampleData> tests{
-      {"TRACE / HTTP/1.1",
-       {"Host: none", "Foo: Bar"},
+      {"OPTIONS / HTTP/1.1",
        200,
-       {{"Content-Type", "message/http"}},
-       "TRACE / HTTP/1.1\r\nFoo: Bar\r\nHost: none\r\n"},
+       {{"Allow", "OPTIONS"}, {"Content-Type", "text/markdown; charset=UTF-8"}},
+       "# Applicable Resource Processors\n\n"
+       " * _OPTIONS_ `^\\*|/.*`\n   no description available\n"},
   };
+
+  httpd::servlet<transport::fake> fakeHandler(
+      httpd::options::resource, httpd::options::options<transport::fake>,
+      httpd::options::method);
 
   for (const auto &tt : tests) {
     http::recorder sess;
     std::smatch matches;
 
     sess.inboundRequest = tt.request;
-    for (const auto &h : tt.headers) {
-      sess.inbound.absorb(h);
-    }
+    sess.connection.processor.servlets.insert(&fakeHandler);
 
-    httpd::trace::trace<transport::fake>(sess, matches);
+    std::string resource = sess.inboundRequest.resource.path();
+    std::regex_match(resource, matches, fakeHandler.resource);
+    httpd::options::options<transport::fake>(sess, matches);
 
     if (sess.status != tt.status) {
-      log << "trace() produced an unexpected status code: " << sess.status
+      log << "options() produced an unexpected status code: " << sess.status
           << ", expected " << tt.status << "\n";
       return false;
     }
     if (sess.header != tt.header) {
-      log << "trace() produced unexpected headers.\n";
+      log << "options() produced unexpected headers.\n";
       return false;
     }
     if (sess.message != tt.message) {
-      log << "trace() produced an unexpected message: '" << sess.message
+      log << "options() produced an unexpected message: '" << sess.message
           << "' expected: '" << tt.message << "'\n";
       return false;
     }
